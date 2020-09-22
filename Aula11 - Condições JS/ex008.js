2cd67126904b2283e4066a801f3a42f8cc1fc4bb
WillDavid <wdma.lic17@uea.edu.br>
2020-09-22 19:06:11 -0400

    add Sistema de Trânsito em Html+Css+JS

---
+++ Aula11 - Condições JS/ex008.js
@@ -0,0 +1,14 @@
+var velocidade = 3.5
+
+
+console.log(`Velocidade: ${velocidade}`)
+
+if(velocidade>80.5){
+    
+    console.log("Acima da velocidade")
+
+}else if(velocidade<10){
+    console.log("Abaixo da velocidade")
+}else{
+    console.log("Velocidade na média")
+}
